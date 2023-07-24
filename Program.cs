0ded7a580d9a36ce6a3b0f653b213d1073996c22
AmirAli-Azimi <amiraliazimi2006@gmail.com>
2023-07-24 20:53:37 +0330

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int num;
+Console.Write("enter a number");
+num=int.Parse(Console.ReadLine());
+if(num>1)
+Console.WriteLine("mosbat number");
+else
+{
+        Console.WriteLine("manfi number");
+}
+Console.ReadKey();
